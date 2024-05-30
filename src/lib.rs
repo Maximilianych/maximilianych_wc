e96bd33a2a7aafc9abb6d9829e489d4eee21c368
Maximilianych <maxgorochov27@gmail.com>
2024-05-31 00:01:58 +0300

    Refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
 use clap::Parser;
-use std::borrow::Cow;
 use std::io::{self, Error, Read};
 use std::fs::File;
 use std::path::PathBuf;
-use encoding_rs::UTF_8;
 use indexmap::IndexMap;
 
 /// wc from Maximilianych
@@ -37,28 +35,30 @@ pub fn run(cli: Cli) {
 
     if cli.file_path.len() >= 1 {
         for path in &cli.file_path {
-            let mut buf = Vec::<u8>::new();
-            let file = get_decoded_file(path, &mut buf).unwrap();
-            result_vec.push(get_for_file(&cli, file));
+            let mut file = String::new();
+            File::open(path).unwrap().read_to_string(&mut file).unwrap();
+            let result = get_for_file(&cli, file);
+            result_vec.push(result);
         }
     } else if cli.file_path.len() < 1 {
-        let mut buff = Vec::<u8>::new();
         let mut stdin = io::stdin();
-        stdin.read_to_end(&mut buff).unwrap();
-        let file = UTF_8.decode(&buff).0;
+        let mut file = String::new();
+        stdin.read_to_string(&mut file).unwrap();
         result_vec.push(get_for_file(&cli, file))
     }
 
+    let path_iter = &mut cli.file_path.iter();
     result_vec.iter().for_each(|res| {
         res.iter().for_each(|(_, v)| print!("{v}\t"));
+        if let Some(path) = path_iter.next() {
+            print!("{}", path.to_str().unwrap());
+        }
         print!("\n")
     });
     result_vec.iter().next().unwrap().iter().for_each(|(k, _)| print!("{k}\t"));
-
-
 }
 
-pub fn get_for_file(cli: &Cli, file: Cow<str>) -> IndexMap<String, usize>{
+pub fn get_for_file(cli: &Cli, file: String) -> IndexMap<String, usize> {
     let mut result = IndexMap::new();
     if cli.get_byte_count {
         if let Ok(byte) = get_byte_count(&file) {
@@ -92,34 +92,26 @@ pub fn get_for_file(cli: &Cli, file: Cow<str>) -> IndexMap<String, usize>{
             result.insert("Lines".to_string(), line);
         }
     }
-
     result
 }
 
-pub fn get_decoded_file<'a>(path: &PathBuf, buf: &'a mut Vec<u8>) -> Result<Cow<'a, str>, Error> {
-    File::open(path)?.read_to_end(buf)?;
-    let file = UTF_8.decode(buf);
-
-    Ok(file.0)
-}
-
-pub fn get_byte_count(file: &Cow<str>) -> Result<usize, Error> {
+pub fn get_byte_count(file: &String) -> Result<usize, Error> {
     Ok(file.len() )
 }
 
-pub fn get_char_count(file: &Cow<str>) -> Result<usize, Error> {
+pub fn get_char_count(file: &String) -> Result<usize, Error> {
     Ok(file.chars().filter(|x| !(*x=='\n') && !(*x=='\r')).count())
 }
 
-pub fn get_word_count(file: &Cow<str>) -> Result<usize, Error> {
+pub fn get_word_count(file: &String) -> Result<usize, Error> {
     Ok(file.split_whitespace().count())
 }
 
-pub fn get_line_count(file: &Cow<str>) -> Result<usize, Error> {
+pub fn get_line_count(file: &String) -> Result<usize, Error> {
     Ok(file.split('\n').count())
 }
 
-pub fn get_all_count(file: &Cow<str>) -> Result<(usize, usize, usize, usize), Error> {
+pub fn get_all_count(file: &String) -> Result<(usize, usize, usize, usize), Error> {
     let bc = file.len();
     let cc = file.chars().filter(|x| !(*x=='\n') && !(*x=='\r')).count();
     let wc = file.split_whitespace().count();
@@ -135,33 +127,37 @@ mod tests {
 
     #[test]
     fn test_get_byte_count() {
-        let mut buf = Vec::<u8>::new();
-        assert_eq!(342181, get_byte_count(&get_decoded_file(&PathBuf::from("test.txt"), &mut buf).unwrap()).unwrap());
+        let mut file = String::new();
+        File::open("test.txt").unwrap().read_to_string(&mut file).unwrap();
+        assert_eq!(342181, get_byte_count(&file).unwrap());
     }
 
     #[test]
     fn test_get_char_count() {
-        let mut buf = Vec::<u8>::new();
-        assert_eq!(325001, get_char_count(&get_decoded_file(&PathBuf::from("test.txt"), &mut buf).unwrap()).unwrap());
+        let mut file = String::new();
+        File::open("test.txt").unwrap().read_to_string(&mut file).unwrap();
+        assert_eq!(325001, get_char_count(&file).unwrap());
 
     }
 
     #[test]
     fn test_get_word_count() {
-        let mut buf = Vec::<u8>::new();
-        assert_eq!(58164, get_word_count(&get_decoded_file(&PathBuf::from("test.txt"), &mut buf).unwrap()).unwrap());
+        let mut file = String::new();
+        File::open("test.txt").unwrap().read_to_string(&mut file).unwrap();
+        assert_eq!(58164, get_word_count(&file).unwrap());
     }
     
     #[test]
     fn test_get_line_count() {
-        let mut buf = Vec::<u8>::new();
-        assert_eq!(7143, get_line_count(&get_decoded_file(&PathBuf::from("test.txt"), &mut buf).unwrap()).unwrap());
+        let mut file = String::new();
+        File::open("test.txt").unwrap().read_to_string(&mut file).unwrap();
+        assert_eq!(7143, get_line_count(&file).unwrap());
     }
 
     #[test]
     fn test_get_all_count() {
-        let mut buf = Vec::<u8>::new();
-        assert_eq!((342181, 325001, 58164, 7143), get_all_count(&get_decoded_file(&PathBuf::from("test.txt"), &mut buf).unwrap()).unwrap());
+        let mut file = String::new();
+        File::open("test.txt").unwrap().read_to_string(&mut file).unwrap();
+        assert_eq!((342181, 325001, 58164, 7143), get_all_count(&file).unwrap());
     }
-
 }
